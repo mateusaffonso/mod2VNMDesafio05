5a7edc97d1b8167b6cf6ce4ce247595f56e62fe9
mateusaffonso <augustomateus@poli.ufrj.br>
2022-01-28 00:19:09 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+ // Crie um array que receba 5 itens e exiba no console.
+
+ let ferramentas = ['martelo', 'chave de fenda', 'prego', 'parafuso', 'bucha']
+ console.log(ferramentas)
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+ferramentas.unshift('metro')
+
+
+
+// Utilize um método para remover o último nome do array.
+
+ferramentas.pop()
+
+
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+ferramentas.push('canalheta', 'tampa cega')
+
+
+// Utilize um método para remover o primeiro nome do array.
+
+ferramentas.splice(0,1)
+
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
